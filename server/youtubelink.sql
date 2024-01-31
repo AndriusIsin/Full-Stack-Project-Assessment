6c35b5d24f0c613f8076c8fd9e25f9bf981ef396
AndriusIsin <isin.andrius@gmail.com>
2024-01-31 19:20:47 +0000

    123

--- server/youtubelink.sql
+++ server/youtubelink.sql
@@ -4,7 +4,7 @@ INSERT INTO videos(id,title,url,rating) VALUES (523427,'The Coding Train','https
 INSERT INTO videos(id,title,url,rating) VALUES (82653,'Mac & Cheese ','https://www.youtube.com/watch?v=FUeyrEN14Rk',2111);
 INSERT INTO videos(id,title,url,rating) VALUES (858566,'Videos for Cats','https://www.youtube.com/watch?v=Dh-ULbQmmF8',11);
 INSERT INTO videos(id,title,url,rating) VALUES (453538,'Chris Brown Soul','https://www.youtube.com/watch?v=dnIiFIddJLo',3211);
-INSERT INTO videos(id,title,url,rating) VALUES (283634,'Coding Adventuree','https://www.youtube.com/watch?v=U4ogK0MIzqk',211);
+INSERT INTO videos(id,title,url,rating) VALUES (283634,'Coding Adventure','https://www.youtube.com/watch?v=U4ogK0MIzqk',211);
 INSERT INTO videos(id,title,url,rating) VALUES (562824,'The Let Out','https://www.youtube.com/watch?v=JWIqrKhP2Kg',111);
 INSERT INTO videos(id,title,url,rating) VALUES (442452,'Tia Tamera','https://www.youtube.com/watch?v=C_yI2959DYU',671);
 INSERT INTO videos(id,title,url,rating) VALUES (536363,'Immaa Get It','https://www.youtube.com/watch?v=6b6K1Umz_ro',76);
