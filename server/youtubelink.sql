2e7f369a0f2a4f5d09bf80b4eec81934e674e55b
AndriusIsin <isin.andrius@gmail.com>
2024-01-31 22:16:13 +0000

    pm2 added

--- server/youtubelink.sql
+++ server/youtubelink.sql
@@ -1,6 +1,6 @@
 create table videos (id SERIAL PRIMARY KEY,, title varchar(50), url varchar(50), rating integer );
 INSERT INTO videos(id,title,url,rating) VALUES (523523,'Never','https://www.youtube.com/watch?v=dQw4w9WgXcQ',3500);
-INSERT INTO videos(id,title,url,rating) VALUES (523427,'The Coding Train','https://www.youtube.com/watch?v=HerCR8bw_GE',230);
+INSERT INTO videos(id,title,url,rating) VALUES (523427,'The Coding Trainn','https://www.youtube.com/watch?v=HerCR8bw_GE',230);
 INSERT INTO videos(id,title,url,rating) VALUES (82653,'Mac & Cheese ','https://www.youtube.com/watch?v=FUeyrEN14Rk',2111);
 INSERT INTO videos(id,title,url,rating) VALUES (858566,'Videoss for Cats','https://www.youtube.com/watch?v=Dh-ULbQmmF8',11);
 INSERT INTO videos(id,title,url,rating) VALUES (453538,'Chris Brown Soul','https://www.youtube.com/watch?v=dnIiFIddJLo',3211);
