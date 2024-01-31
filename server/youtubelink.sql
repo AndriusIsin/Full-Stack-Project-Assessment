babfeb2fb90a0a6d9e0758f782c8140634cad370
AndriusIsin <isin.andrius@gmail.com>
2024-01-31 20:21:41 +0000

    2

--- server/youtubelink.sql
+++ server/youtubelink.sql
@@ -2,7 +2,7 @@ create table videos (id SERIAL PRIMARY KEY,, title varchar(50), url varchar(50),
 INSERT INTO videos(id,title,url,rating) VALUES (523523,'Never','https://www.youtube.com/watch?v=dQw4w9WgXcQ',3500);
 INSERT INTO videos(id,title,url,rating) VALUES (523427,'The Coding Train','https://www.youtube.com/watch?v=HerCR8bw_GE',230);
 INSERT INTO videos(id,title,url,rating) VALUES (82653,'Mac & Cheese ','https://www.youtube.com/watch?v=FUeyrEN14Rk',2111);
-INSERT INTO videos(id,title,url,rating) VALUES (858566,'Videos for Cats','https://www.youtube.com/watch?v=Dh-ULbQmmF8',11);
+INSERT INTO videos(id,title,url,rating) VALUES (858566,'Videoss for Cats','https://www.youtube.com/watch?v=Dh-ULbQmmF8',11);
 INSERT INTO videos(id,title,url,rating) VALUES (453538,'Chris Brown Soul','https://www.youtube.com/watch?v=dnIiFIddJLo',3211);
 INSERT INTO videos(id,title,url,rating) VALUES (283634,'Coding Adventure','https://www.youtube.com/watch?v=U4ogK0MIzqk',211);
 INSERT INTO videos(id,title,url,rating) VALUES (562824,'The Lets Out','https://www.youtube.com/watch?v=JWIqrKhP2Kg',111);
