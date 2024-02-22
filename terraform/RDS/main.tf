dc98761841fde0e68ef335a52355da4fd81e8db4
AndriusIsin <isin.andrius@gmail.com>
2024-02-22 22:26:07 +0000

    finished

--- terraform/RDS/main.tf
+++ terraform/RDS/main.tf
@@ -1,49 +1,34 @@
-terraform {
-  required_providers {
-    aws = {                                     // Define required AWS provider
-      source  = "hashicorp/aws"                
-      version = "~> 5.37.0"                     
-    }
-  }
-  required_version = ">= 1.0.0"                 // Terraform version constraint
+# Define the AWS provider and region
+provider "aws" {
+  region = "eu-west-2"
 }
 
-provider "aws" {                                 // Configure AWS provider
-  region = "eu-west-2"                          
+# Use an existing VPC
+data "aws_vpc" "existing" {
+  id = "vpc-0046d4dffe9dcc0be"
 }
 
-resource "aws_security_group" "video_security" { // Define AWS security group resource
-  name        = "video_andrius_security_group"  
-  description = "Allow ports 8080 and 4000"     
-
-  ingress {                                     // Define ingress rules
-    from_port   = 8080                           
-    to_port     = 8080                          
-    protocol    = "tcp"                         
-    cidr_blocks = ["0.0.0.0/0"]                 
-  }
-
-  ingress {                                     // Define another ingress rule
-    from_port   = 4000                          
-    to_port     = 4000                          
-    protocol    = "tcp"                         
-    cidr_blocks = ["0.0.0.0/0"]                  
-  }
-
-  egress {                                      // Define egress rule
-    from_port   = 0                            
-    to_port     = 0                             
-    protocol    = "-1"                        
-    cidr_blocks = ["0.0.0.0/0"]           
-  }
+# Use an existing subnet group
+data "aws_db_subnet_group" "existing" {
+  name = "default-vpc-0046d4dffe9dcc0be"
 }
 
-resource "aws_instance" "video_andrius" {       // Define AWS EC2 instance resource
-  instance_type          = "t2.micro"          
-  ami                    = "ami-09d6bbc1af02c2ca1"  
-  vpc_security_group_ids = [aws_security_group.video_security.id]  // Attach security group to the instance
-
-  tags = {                                     
-    Name = "video_andrius_instance"             // Name tag
-  }
+# Create an RDS instance using the existing security group
+resource "aws_db_instance" "videodb" {
+  identifier                  = "videodb"
+  allocated_storage           = 20
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "15.5"
+  instance_class              = "db.t3.micro"
+  publicly_accessible         = true 
+  db_subnet_group_name        = data.aws_db_subnet_group.existing.name
+  vpc_security_group_ids      = ["sg-0558f33e4502e9ccc"] 
+  backup_retention_period     = 0
+  backup_window               = "00:00-00:30"
+  maintenance_window          = "mon:01:48-mon:02:18"
+  auto_minor_version_upgrade  = true
+  username                    = "andrius"   
+  password                    = "password"   
+  parameter_group_name        = "default.postgres15"
 }
